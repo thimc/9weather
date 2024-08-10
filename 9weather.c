ebfd206c96ec5448b02ada5ed92502b1a0f69f2a
Thim Cederlund <thim@cederlund.de>
2024-08-10 16:28:32 +0200

    feat: use environment variables and true color images
    We are now able to set the API key with the `openweathermap`
    environment variable, and we can also set the ZIP/city with the
    `ZIP` variable.
    
    According to the OpenWeather API the icon changes during night time
    to greyscaled ones. While I was looking through the code I noticed
    that I had shuffled the arguments for the `draw()` around which
    made them greyscaled all the time.
    
    Reference: https://openweathermap.org/weather-conditions#Icon-list

--- 9weather.c
+++ 9weather.c
@@ -12,45 +12,26 @@ enum {
 	Eresize,
 	Etimer,
 };
+
+enum {
+	Imgsize = 100,		/* images from openweathermap are 100x100 pixels */
+	Delay = 5*(60*1000),	/* default delay is 5 minutes */
+};
+
 Mousectl *mctl;
 Keyboardctl *kctl;
 
-Image* icon;
-Image* background;
-Image* csun;
-Font* defaultfont;
-
-#define ICONDIM 100
+Image *background, *icon;
 #define MAXSIZ 4096
 
-int unitflag;
-int delay;
-
-char *apikey;
-char *zip;
-
-char city[25];
-char description[25];
-char temperature[25];
-char iconid[5];
-
-void usage(void);
-void ekeyboard(Rune k);
-double round(double n);
-int webclone(int *conn);
-void writeurl(int fd, char* url);
-char *readbody(int c);
-void polldata(void);
-void mkiconfile(void);
-void timerproc(void *c);
-void eresized(int new);
-void redraw(void);
-
+int delay = Delay, unitflag;
+char *apikey, *zip;
+char city[25], description[25], temperature[25], iconid[5];
 
 void
 usage(void)
 {
-	print("usage: %s [-d delay] [-i] [-z zip,country] [-f font] [-k apikey]\n", argv0);
+	print("usage: %s [-d seconds] [-i] [-z zip,country] [-k apikey]\n", argv0);
 	threadexitsall("usage");
 }
 
@@ -72,7 +53,7 @@ round(double n)
 	
 	f = floor(n);
 	c = ceil(n);
-	if(n-f > c-n)
+	if(n - f > c - n)
 		return f;
 	return c;
 }
@@ -101,7 +82,7 @@ writeurl(int fd, char* url)
 	char buf[256];
 	int n;
 
-	snprint(buf, sizeof buf, "url %s", url);
+	snprint(buf, sizeof(buf), "url %s", url);
 	n = strlen(buf);
 	if(write(fd, buf, n) != n)
 		sysfatal("write: %r");
@@ -110,13 +91,13 @@ writeurl(int fd, char* url)
 char*
 readbody(int c)
 {
-	char buf[256], body[MAXSIZ];
+	char buf[256], body[4096];
 	int n, fd;
 
-	snprint(buf, sizeof buf, "/mnt/web/%d/body", c);
+	snprint(buf, sizeof(buf), "/mnt/web/%d/body", c);
 	if((fd = open(buf, OREAD)) < 0)
 		sysfatal("open %s: %r", buf);
-	if((n = readn(fd, body, sizeof body)) < 0)
+	if((n = readn(fd, body, sizeof(body))) < 0)
 		sysfatal("readn: %r");
 	close(fd);
 	body[n] = '\0';
@@ -129,40 +110,33 @@ polldata(void)
 {
 	JSON *obj, *desc, *ico, *temp;
 	JSONEl *arr;
-	char *buf, *u;
+	char u[256], *buf;
 	int conn, ctlfd;
 
-	u = malloc(sizeof(char) * 256);
-	if(u == nil)
-		sysfatal("malloc: %r");
-
-	snprint(u, 256, "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=%s&mode=json", zip, apikey, (unitflag ? "imperial" : "metric"));
+	snprint(u, sizeof(u), "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=%s&mode=json",
+		zip, apikey, (unitflag ? "imperial" : "metric"));
 	ctlfd = webclone(&conn);
 	writeurl(ctlfd, u);
 	buf = readbody(conn);
 	close(ctlfd);
-	free(u);
 
-	obj = jsonparse(buf);
-	if(obj == nil)
+	if((obj = jsonparse(buf)) == nil)
 		sysfatal("jsonparse: %r");
 
 	for(JSONEl *json = obj->first; json != nil; json = json->next){
-		if(strcmp(json->name,"name")==0)
-			snprint(city, sizeof city, json->val->s);
+		if(strcmp(json->name,"name") == 0)
+			snprint(city, sizeof(city), json->val->s);
 		if(strcmp(json->name, "weather") == 0){
 			arr = json->val->first;
 			if(desc = jsonbyname(arr->val, "description"))
-				snprint(description, sizeof description, desc->s);
+				snprint(description, sizeof(description), desc->s);
 			if(ico = jsonbyname(arr->val, "icon"))
-				snprint(iconid, sizeof iconid, ico->s);
+				snprint(iconid, sizeof(iconid), ico->s);
 		}
 		if(strcmp(json->name, "main") == 0){
 			if(temp = jsonbyname(json->val, "temp")){
-				snprint(temperature, sizeof temperature, "%s%d°%s",
-					(temp->n > 0 ? "+" : ""),
-					(int)round(temp->n),
-					(unitflag ? "F" : "C"));
+				snprint(temperature, sizeof(temperature), "%s%d°%s",
+					(temp->n > 0 ? "+" : ""), (int)round(temp->n), (unitflag ? "F" : "C"));
 			}
 		}
 	}
@@ -172,14 +146,10 @@ polldata(void)
 void
 mkiconfile(void)
 {
-	char *body, *url, *arg[4], *cmd;
+	char cmd[30], url[50], *arg[4], *body;
 	int conn, ctlfd, f, ifd;
 
-	url = malloc(50 * sizeof(char));
-	if(url == nil)
-		sysfatal("malloc: %r");
-	snprint(url, 50, "http://openweathermap.org/img/wn/%s@2x.png", iconid);
-
+	snprint(url, sizeof(url), "http://openweathermap.org/img/wn/%s@2x.png", iconid);
 	ctlfd = webclone(&conn);
 	writeurl(ctlfd, url);
 	body = readbody(conn);
@@ -190,10 +160,7 @@ mkiconfile(void)
 	write(f, body, MAXSIZ);
 	close(f);
 
-	cmd = malloc(30 * sizeof(char));
-	if(cmd == nil)
-		sysfatal("malloc: %r");
-	snprint(cmd, 30, "png -c icon.png > icon");
+	snprint(cmd, sizeof(cmd), "png -tc icon.png > icon");
 	arg[0] = "rc";
 	arg[1] = "-c";
 	arg[2] = cmd;
@@ -208,7 +175,6 @@ mkiconfile(void)
 	default:
 		waitpid();
 	}
-	free(cmd);
 
 	if(icon)
 		freeimage(icon);
@@ -234,13 +200,6 @@ timerproc(void *c)
 }
 
 void
-eresize(void)
-{
-	redraw();
-	flushimage(display, 1);
-}
-
-void
 redraw(void)
 {
 	Point p, m;
@@ -249,18 +208,13 @@ redraw(void)
 	draw(screen, screen->r, background, nil, ZP);
 	p = Pt(screen->r.min.x, screen->r.min.y);
 	m = Pt(screen->r.max.x-p.x, screen->r.max.y-p.y);
-	txtoffset = m.y-(3*defaultfont->height);
+	txtoffset = m.y-(3*font->height);
 
-	string(screen, Pt(p.x+ICONDIM+1, p.y+(txtoffset/2)), display->black, ZP, defaultfont, city);
-	string(screen, Pt(p.x+ICONDIM+1, p.y+(txtoffset/2)+defaultfont->height), display->black, ZP, defaultfont, description);
-	string(screen, Pt(p.x+ICONDIM+1, p.y+(txtoffset/2)+(defaultfont->height*2)), display->black, ZP, defaultfont, temperature);
-	draw(screen, Rect(p.x, p.y+(m.y/2)-(ICONDIM/2), p.x+ICONDIM, p.y+(m.y/2)+(ICONDIM/2)), display->transparent, icon, ZP);
-/*
-	string(screen, Pt(p.x+ICONDIM+1, p.y+defaultfont->height), display->black, ZP, defaultfont, city);
-	string(screen, Pt(p.x+ICONDIM+1, p.y+(defaultfont->height*2)), display->black, ZP, defaultfont, description);
-	string(screen, Pt(p.x+ICONDIM+1, p.y+(defaultfont->height*3)), display->black, ZP, defaultfont, temperature);
-	draw(screen, Rect(p.x, p.y, p.x+ICONDIM, p.y+ICONDIM), display->transparent, icon, ZP);
-*/
+	string(screen, Pt(p.x+Imgsize+1, p.y+(txtoffset/2)), display->black, ZP, font, city);
+	string(screen, Pt(p.x+Imgsize+1, p.y+(txtoffset/2)+font->height), display->black, ZP, font, description);
+	string(screen, Pt(p.x+Imgsize+1, p.y+(txtoffset/2)+(font->height*2)), display->black, ZP, font, temperature);
+	draw(screen, Rect(p.x, p.y+(m.y/2)-(Imgsize/2), p.x+Imgsize, p.y+(m.y/2)+(Imgsize/2)), icon, nil, ZP);
+
 	flushimage(display, 1);
 }
 
@@ -276,41 +230,19 @@ threadmain(int argc, char *argv[])
 		{ nil, nil, CHANRCV },
 		{ nil, nil, CHANEND },
 	};
-	char *font;
 
-	unitflag = 0;
-	delay = 5 * (60 * 1000);
-	font = getenv("font");
-	if(font == nil)
-		sysfatal("getenv: no $font set");
-
-	ARGBEGIN {
-	case 'd':
-		delay = atoi(EARGF(usage())) * 1000;
-		break;
-	case 'f':
-		font = EARGF(usage());
-		break;
-	case 'i':
-		unitflag++;
-		break;
-	case 'k':
-		apikey = EARGF(usage());
-		break;
-	case 'z':
-		zip = EARGF(usage());
-		break;
-	case 'h':
-		usage();
-		break;
-	} ARGEND;
+	apikey = getenv("openweathermap");
+	zip = getenv("ZIP");
+	ARGBEGIN{
+	case 'd': delay = atoi(EARGF(usage())) * 1000; break;
+	case 'i': unitflag++; break;
+	case 'k': apikey = EARGF(usage()); break;
+	case 'z': zip = EARGF(usage()); break;
+	case 'h': usage(); break;
+	}ARGEND;
 
 	if(!apikey || !zip)
 		usage();
-
-	if(access(font, AREAD) < 0)
-		sysfatal("access: %s: %r", font);
-
 	if(initdraw(nil, nil, argv0) < 0)
 		sysfatal("initdraw: %r");
 	display->locking = 0;
@@ -326,7 +258,6 @@ threadmain(int argc, char *argv[])
 	proccreate(timerproc, alts[Etimer].c, 1024);
 
 	background = allocimagemix(display, DPalebluegreen, DWhite);
-	defaultfont = openfont(display, font);
 	goto timer;
 
 	for(;;){
@@ -337,7 +268,7 @@ threadmain(int argc, char *argv[])
 		case Eresize:
 			if(getwindow(display, Refnone) < 0)
 				sysfatal("getwindow: %r");
-			eresize();
+			redraw();
 			break;
 		case Etimer:
 timer:
